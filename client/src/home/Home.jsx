b56f5093322d8487aec10d964b7fd3a8317efe41
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-05-24 13:26:38 +0200

    added new styles in css

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -7,9 +7,27 @@ const Home = ()=>{
 const backgroundContext = useBackground()
 
 return(
-<div className="mainContener" style={backgroundContext.bg.background1}>
-  
-    {/* <h1>DOKĄD SIĘ WYBIERASZ ?</h1> */}
+<div className="mainContener" style={backgroundContext.bg.light}>
+  <div className="titleContener">
+      <h1>DOKĄD SIĘ WYBIERASZ??</h1>
+  </div>
+  <div className="inputContener">
+      <div>
+      <label htmlFor="tripStart">
+      <input name="tripStart" id="tripStart" className="itripInputs" type="text" placeholder="WYZNACZ POCZĄTEK TRASY"/>
+      <span className="spanAnime">WYZNACZ POCZĄTEK TRASY</span>
+      </label>
+      </div>
+      <div className="tripEndDiv">
+      <label htmlFor="tripEnd">
+      <input name="tripEnd" id="tripEnd" type="text" className="itripInputs" placeholder="WYZNACZ KONIEC TRASY"/>
+      <span className="spanAnime">WYZNACZ KONIEC TRASY</span>
+      </label>
+      </div>
+      <div>
+          <button className="btn" type="button">SPRAWDŹ</button>
+      </div>
+  </div>
 </div>
 )
 }
--- client/src/styleConrxt/StyleContext.jsx
+++ client/src/styleConrxt/StyleContext.jsx
@@ -1,15 +1,20 @@
-import React, {useState ,createContext, useContext} from "react";
+import React, {useState ,createContext, useContext, useEffect} from "react";
+import WebFont from 'webfontloader';
 import image1 from "../img/map-and-navigation.png"
 
+
 const backGrounds = {
-    background1:{
+    light:{
+               fontFamily: "Oswald, sans-serif",
+               color: "blueviolet",
+               fontWeight: "bold",
                backgroundImage: `url(${image1})`,
                backgroundSize: 'cover',
                backgroundRepeat: 'no-repeat',
                backgroundAttachment: "fixed",
                backgroundPosition: "center",
                height: "100vh"
-    }
+    },
 }
 
 const BackgroundContext = createContext();
